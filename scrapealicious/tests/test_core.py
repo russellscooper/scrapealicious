245f6bd9dccceb48a529337481e56f750f918766
samuel cooper <cooper.src261@gmail.com>
2023-11-27 06:35:48 -0800

    Fixed unit tests conventions. Reran tests with all results ok.

--- scrapealicious/tests/test_core.py
+++ scrapealicious/tests/test_core.py
@@ -3,7 +3,7 @@ import unittest
 from app.core.tools import Scanner, Security
 
 class ScannerTest(unittest.TestCase):
-    def exctractor_test(self):
+    def test_extractor(self):
         '''
         Verify that the exctractor method can exctract URLs from target URL.
         '''
@@ -30,10 +30,10 @@ class ScannerTest(unittest.TestCase):
         self.assertTrue(len(valid_urls) > 0)
 
 class SecurityTest(unittest.TestCase):
-    def setTest(self):
+    def setUp(self):
         self.security_instance = Security(secure=True)
 
-    def secure_string_test(self):
+    def test_secure_string(self):
         '''
         test to see if method removes white spaces and threat characters.
         '''
@@ -41,7 +41,7 @@ class SecurityTest(unittest.TestCase):
         expect = 'test input'
         self.assertEqual(self.security_instance.secure_string(input_string), expect)
 
-    def max_length_test(self):
+    def test_max_length(self):
         '''
         Test if method raises value error for strings exceeding maximum length
         '''
@@ -49,10 +49,10 @@ class SecurityTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.security_instance.secure_string(input_string)
 
-    def valid_input_test(self):
+    def test_valid_input(self):
         invalid_input = 12345
         with self.assertRaises(ValueError):
             self.security_instance.secure_string(invalid_input)
-            
+
 if __name__ == '__main__':
     unittest.main()
